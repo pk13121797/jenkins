52daa2fc9d641565a4179b63003928c26b05bd86
user <kotowich.pavel@yandex.by>
2024-06-19 02:57:34 +0300

    initial commit

---
+++ src/main/java/by/pavvel/config/WebConfig.java
@@ -0,0 +1,21 @@
+package by.pavvel.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan("by.pavvel")
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    private final ApplicationContext applicationContext;
+
+    @Autowired
+    public WebConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+}
---
+++ src/main/java/by/pavvel/controller/HomeController.java
@@ -0,0 +1,33 @@
+package by.pavvel.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/")
+public class HomeController {
+
+    @GetMapping
+    public ResponseEntity<String> home() {
+
+        String s1 = """
+                [
+                {"id" : 1,"version" : "version-1 \uD83E\uDD72"}
+                ]
+                """;
+
+        String s2 = """
+                [
+                {"id" : 2,"version" : "version-2 \uD83D\uDE0A"}
+                ]
+                """;
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add("Content-Type", "application/json; charset=UTF-8");
+        return new ResponseEntity<>(s1, responseHeaders, HttpStatus.OK);
+    }
+}
